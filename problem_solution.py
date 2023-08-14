09ff3e815a0e75339c0204d603d4d9d1c3605a5c
Ritesh Dubey <36127401+ritesd@users.noreply.github.com>
2023-08-14 14:15:41 +0530

    Top K Frequent Elements

--- problem_solution.py
+++ problem_solution.py
@@ -148,3 +148,29 @@ class Solution:
                 count[ord(c) - ord('a')] += 1
             ans[tuple(count)].append(st)
         return ans.values()
+    
+
+"""
+Top K Frequent Elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+Type: Medium
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ans = collections.defaultdict(int)
+        for num in nums:
+            ans[num] += 1
+        return sorted(ans, key=lambda x: ans[x], reverse=True)[0:k]
