d7728a1d7996612dd0ed8154b9411e004a5bbf0f
Ritesh Dubey <36127401+ritesd@users.noreply.github.com>
2023-08-20 18:44:51 +0530

    problem solution

--- problem_solution.py
+++ problem_solution.py
@@ -174,3 +174,107 @@ class Solution:
         for num in nums:
             ans[num] += 1
         return sorted(ans, key=lambda x: ans[x], reverse=True)[0:k]
+
+"""
+Valid Sudoku
+type: Medium
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+"""
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        square = collections.defaultdict(set) ## r/3, c/3
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in square[(r//3, c//3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                square[(r//3,c//3)].add(board[r][c])
+        return True
+
+"""
+Encode and Decode Strings
+
+Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+Please implement encode and decode
+
+Only $39.9 for the "Twitter Comment System Project Practice" within a limited time of 7 days!
+
+Example
+Example1
+
+Input: ["lint","code","love","you"]
+Output: ["lint","code","love","you"]
+Explanation:
+One possible encode method is: "lint:;code:;love:;you"
+Example2
+
+Input: ["we", "say", ":", "yes"]
+Output: ["we", "say", ":", "yes"]
+Explanation:
+One possible encode method is: "we:;say:;:::;yes"
+
+"""
+
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # write your code here
+        return ";:".join(map(lambda x: x[::-1], strs)))
+
+    """
+    @param: str: A string
+    @return: decodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+        return list(map(lambda x: x[::-1], str.split(';:')))
